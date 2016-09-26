7bc3b280f1cc55a3924275a8604c8df438394233
bandworthy <s3240684@student.rmit.edu.au>
2016-09-26 18:05:28 +1000

    check box test messages all added.

--- app/src/main/java/group4/programmingproject1/Settings.java
+++ app/src/main/java/group4/programmingproject1/Settings.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.SoundEffectConstants;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
@@ -12,7 +13,7 @@ import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
 
-    CheckBox textBox,emailBox,soundBox,videoBox,callBox;
+    CheckBox textBox,emailBox,soundBox,videoBox,callBox,mapgpsBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -69,11 +70,15 @@ public class Settings extends AppCompatActivity {
             {
                 if (emailBox.isChecked())
                 {
+                    //play click sound
+                    //v.playSoundEffect(android.view.SoundEffectConstants.CLICK);
                     Toast.makeText(Settings.this,"Send Email Message Checked",Toast.LENGTH_SHORT).show();
                     //settings value on needs to be saved
                 }
                 else
                 {
+                    //play click sound
+                    //v.playSoundEffect(android.view.SoundEffectConstants.CLICK);
                     Toast.makeText(Settings.this,"Send Email Message Unchecked",Toast.LENGTH_SHORT).show();
                     //setting values off needs to be saved
                 }
@@ -82,7 +87,97 @@ public class Settings extends AppCompatActivity {
 
 
 
+        soundBox = (CheckBox)findViewById(R.id.Checkbox_RecSound);
 
+        //SoundMsg CheckBox
+
+        soundBox.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                if (soundBox.isChecked())
+                {
+                    Toast.makeText(Settings.this,"Send record sound Checked",Toast.LENGTH_SHORT).show();
+                    //settings value on needs to be saved
+                }
+                else
+                {
+                    Toast.makeText(Settings.this,"Send record sound Unchecked",Toast.LENGTH_SHORT).show();
+                    //setting values off needs to be saved
+                }
+            }
+        });
+
+        videoBox = (CheckBox)findViewById(R.id.Checkbox_RecVideo);
+
+        //SoundMsg CheckBox
+
+        videoBox.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                if (videoBox.isChecked())
+                {
+                    Toast.makeText(Settings.this,"Send record video Checked",Toast.LENGTH_SHORT).show();
+                    //settings value on needs to be saved
+                }
+                else
+                {
+                    Toast.makeText(Settings.this,"Send record video Unchecked",Toast.LENGTH_SHORT).show();
+                    //setting values off needs to be saved
+                }
+            }
+        });
+
+        callBox = (CheckBox)findViewById(R.id.Checkbox_EmeNum);
+
+        //SoundMsg CheckBox
+
+        callBox.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                if (callBox.isChecked())
+                {
+                    Toast.makeText(Settings.this,"Send record sound Checked",Toast.LENGTH_SHORT).show();
+                    //settings value on needs to be saved
+                }
+                else
+                {
+                    Toast.makeText(Settings.this,"Send record sound Unchecked",Toast.LENGTH_SHORT).show();
+                    //setting values off needs to be saved
+                }
+            }
+        });
+
+        mapgpsBox = (CheckBox)findViewById(R.id.CheckBox_GPS);
+
+        //SoundMsg CheckBox
+
+        mapgpsBox.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                if (mapgpsBox.isChecked())
+                {
+                    Toast.makeText(Settings.this,"Send Gmap/GPS Checked",Toast.LENGTH_SHORT).show();
+                    //settings value on needs to be saved
+                }
+                else
+                {
+                    Toast.makeText(Settings.this,"Send Gmap/GPS Unchecked",Toast.LENGTH_SHORT).show();
+                    //setting values off needs to be saved
+                }
+            }
+        });
 
     }
 
