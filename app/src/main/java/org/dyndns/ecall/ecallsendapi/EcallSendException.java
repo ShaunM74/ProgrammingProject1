83323d5f3f18899b0bb766c21c895be23253e96d
Shaun M <shaun.mc74@yahoo.com>
2016-10-14 09:40:01 +1100

    Added Ecall API files

---
+++ app/src/main/java/org/dyndns/ecall/ecallsendapi/EcallSendException.java
@@ -0,0 +1,14 @@
+package org.dyndns.ecall.ecallsendapi;
+
+
+/**
+ * Created by s3221035 on 5/10/16.
+ */
+
+public class EcallSendException extends Exception {
+
+    public EcallSendException(String message) {
+        super(message);
+    }
+
+}
