cbc36cb8034b9f70b35452a7f0ad4a0d1fa45422
Shaun M <shaun.mc74@yahoo.com>
2016-10-21 13:53:01 +1100

    First draft of SMS Despatcher in EcallMessageDespatcherViaSMS

--- app/src/main/java/org/dyndns/ecall/ecallsendapi/EcallMessageDespatcherViaSMS.java
+++ app/src/main/java/org/dyndns/ecall/ecallsendapi/EcallMessageDespatcherViaSMS.java
@@ -1,12 +1,21 @@
 package org.dyndns.ecall.ecallsendapi;
 
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.Toast;
+import android.content.Context;
+
 import org.dyndns.ecall.ecalldataapi.EcallAlert;
+import org.json.JSONObject;
 
 /**
  * Created by bajaques on 4/10/2016.
  */
 
 public class EcallMessageDespatcherViaSMS extends EcallMessageDespatcher {
+
+    private String phoneNo=null;
+    private String message=null;
 
     public EcallMessageDespatcherViaSMS(EcallAlert alert)
     {
@@ -26,20 +35,48 @@ public class EcallMessageDespatcherViaSMS extends EcallMessageDespatcher {
 
         public Boolean connectToService ()
         {
-            return false;
+            return true;
         }
 
         @Override
     /* get message ready for depatch */
         public  Boolean prepareMessage () {
-            return false;
+
+            phoneNo = getContact().getPhone();
+            try {
+                JSONObject mainObject = new JSONObject(getAlert().getPayload());
+                message =mainObject.getString("Message") + " Lat:"+mainObject.getString("Latitude")+
+                        " Long:"+mainObject.getString("Longitude")+" Date:"+mainObject.getString("Date")+
+                        " Time:"+mainObject.getString("Time");
+
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            return true;
         }
 
         @Override
     /* send the message  */
         public  Boolean despatchMessage ()
         {
-            return false;
+            Log.i("Send SMS", "");
+
+
+            try {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            }
+
+            catch (Exception e) {
+
+                e.printStackTrace();
+                return false;
+            }
+
+
+            return true;
         }
 
 
